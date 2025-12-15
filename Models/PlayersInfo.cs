c181231e0a2b6eed70d5f52a7a91d6fdd50f7a70
pounceinwindow <adk03042006@gmail.com>
2025-12-15 23:16:14 +0300

    clean;
    git commit -m clean

--- Models/PlayersInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SavePuffle.Models;
-
-public sealed class PlayerInfo
-{
-    public string Nickname { get; set; } = "";
-    public string Email { get; set; } = "";
-    public bool IsHost { get; set; }
-    public bool IsReady { get; set; }
-}
