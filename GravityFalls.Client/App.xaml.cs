befd8b8b8870057cabb841c146446d7d4ec8605a
pounceinwindow <adk03042006@gmail.com>
2025-12-15 23:17:57 +0300

    clean

---
+++ GravityFalls.Client/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using GravityFallsClient;
+
+namespace SavePuffle;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+        MainPage = new AppShell();
+    }
+}
---
+++ GravityFalls.Client/Pages/MainMenuPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace GravityFallsClient.Pages;
+
+public partial class MainMenuPage : ContentPage
+{
+    public MainMenuPage()
+    {
+        InitializeComponent();
+        StartButton.Clicked += async (_, __) =>
+        {
+            await Shell.Current.GoToAsync(nameof(CharacterSelectionPage));
+        };
+    }
+
+
+}
+//TODO: StartButton.Clicked 
+//возьмёт значения из Entry(может по визуальному дереву или x:Name)
+//подключится к серверу
+//перейдёт на CharacterSelectionPage
+//RulesButton → покажет правила
+//ExitButton → закроет приложение
