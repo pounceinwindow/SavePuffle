0922e7d45d3906e483bcf3f0d7b2a43197a3ff35
pounceinwindow <adk03042006@gmail.com>
2025-12-27 11:47:31 +0300

    thats all.. i think so..

--- GravityFalls.Server/obj/Debug/net9.0/GravityFalls.Server.AssemblyInfo.cs
+++ GravityFalls.Server/obj/Debug/net9.0/GravityFalls.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GravityFalls.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+098094b380ac31f6a4a1d2466785330c2e3a9548")]
 [assembly: System.Reflection.AssemblyProductAttribute("GravityFalls.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GravityFalls.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- GravityFalls.Shared/obj/Debug/net9.0/GravityFalls.Shared.AssemblyInfo.cs
+++ GravityFalls.Shared/obj/Debug/net9.0/GravityFalls.Shared.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GravityFalls.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+098094b380ac31f6a4a1d2466785330c2e3a9548")]
 [assembly: System.Reflection.AssemblyProductAttribute("GravityFalls.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GravityFalls.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
